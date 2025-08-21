91b7603a6e9076615eb2ebbd6222af62f7c8384e
LycollyX <urutelimaratelio@gmail.com>
2025-08-21 16:18:00 +0900

    adjusting true PATH

--- server-install-ubuntu.sh
+++ server-install-ubuntu.sh
@@ -8,7 +8,7 @@ echo "Installing prerequisites..."
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install -y steamcmd
-sudo adduser steam
+sudo useradd steam
 
 # ASAサーバーのインストールディレクトリ
 ASA_SERVER_DIR="/home/steam/Steam/"
@@ -20,7 +20,7 @@ ASA_APPID=2430930
 # --- ステップ3: ASAサーバーのダウンロード ---
 # ASAサーバーをダウンロード
 echo "Downloading ASA dedicated server..."
-steamcmd +force_install_dir /home/steam/ +login anonymous +app_update "$ASA_APPID" validate +quit
+steamcmd +force_install_dir /home/steam/Steam/steamapps/common/ +login anonymous +app_update "$ASA_APPID" validate +quit
 
 # --- ステップ2: Protonのダウンロード ---
 # Protonをダウンロード (Proton 8.0を使用)
@@ -28,13 +28,13 @@ echo "Downloading Proton..."
 cd ~
 curl -L -o GE-Proton8-21.tar.gz https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-21/GE-Proton8-21.tar.gz
 tar -xf GE-Proton8-21.tar.gz
-sudo mv GE-Proton8-21 /home/steam/compatibilitytools.d/
-sudo chown -R steam:steam /home/steam/compatibilitytools.d/GE-Proton8-21
-sudo chmod -R +x /home/steam/compatibilitytools.d/GE-Proton8-21/
+sudo mv GE-Proton8-21 /home/steam/Steam/compatibilitytools.d/
+sudo chown -R steam:steam /home/steam/Steam/compatibilitytools.d/GE-Proton8-21
+sudo chmod -R +x /home/steam/Steam/compatibilitytools.d/GE-Proton8-21/
 
 # --- ステップ4: systemdサービスのインストール ---
 echo "Copying systemd service file from repository..."
-sudo cp ./systemd/asa-server.service /etc/systemd/system/asa-server.service
+sudo cp ./systemd/ark-island.service /etc/systemd/system/ark-island.service
 
 # サービスを有効化し、起動
 echo "Enabling and starting the service..."
